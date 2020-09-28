d838217480751184e08f79700137d90e59c3496b
Nana Essandoh <nanaessandoh@gmail.com>
2020-09-28 09:16:42 -0230

    Update forecast.js

--- weather_app/scripts/forecast.js
+++ weather_app/scripts/forecast.js
@@ -2,9 +2,9 @@ class Forecast {
   constructor() {
     this.apiKey = "R38hBD8omwfEVu6S8eYrHD9hcngZGQOG";
     this.weatherURI =
-      "http://dataservice.accuweather.com/currentconditions/v1/";
+      "https://dataservice.accuweather.com/currentconditions/v1/";
     this.cityURI =
-      "http://dataservice.accuweather.com/locations/v1/cities/search";
+      "https://dataservice.accuweather.com/locations/v1/cities/search";
   }
 
   async updateCity(city) {
